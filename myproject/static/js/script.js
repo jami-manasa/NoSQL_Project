aceca1a41893c48e3cf492b6147cf21ee6b8f659
J  Manasa <64403837+jami-manasa@users.noreply.github.com>
2025-04-11 23:58:31 -0500

    mongo db atlas

--- myproject/static/js/script.js
+++ myproject/static/js/script.js
@@ -2,4 +2,15 @@ function togglePassword() {
     const pw = document.getElementById('password');
     pw.type = pw.type === 'password' ? 'text' : 'password';
   }
+
+  
+  // home page
+  function toggleChat() {
+    const chat = document.getElementById("chatWindow");
+    chat.style.display = chat.style.display === "flex" ? "none" : "flex";
+  }
+  
+  function openPostModal() {
+    alert("Open modal to submit a story/post/photo/video.");
+  }
   
